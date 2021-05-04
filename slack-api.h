2d92494acbe74ddca62f11e5de1f7af152298c16
Dylan Simon <dylan@dylex.net>
2021-05-04 09:30:03 -0400

    use existing G_STRINGIFY macro for SLACK_LIMIT_ARG

--- slack-api.h
+++ slack-api.h
@@ -14,8 +14,7 @@ void slack_api_get(SlackAccount *sa, SlackAPICallback *callback, gpointer user_d
 void slack_api_post(SlackAccount *sa, SlackAPICallback *callback, gpointer user_data, const char *endpoint, /* const char *query_param1, const char *query_value1, */ ...) G_GNUC_NULL_TERMINATED;
 void slack_api_disconnect(SlackAccount *sa);
 
-#define SLACK_LIMIT_ARG2(COUNT)		"limit", #COUNT
-#define SLACK_LIMIT_ARG(COUNT)		SLACK_LIMIT_ARG2(COUNT)
+#define SLACK_LIMIT_ARG(COUNT)		"limit", G_STRINGIFY(COUNT)
 
 #define SLACK_PAGINATE_LIMIT_COUNT	500
 #define SLACK_PAGINATE_LIMIT_ARG	SLACK_LIMIT_ARG(SLACK_PAGINATE_LIMIT_COUNT)
