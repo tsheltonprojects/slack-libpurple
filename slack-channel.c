tsheltonprojects/slack-libpurple

commit d9c6acd52c3b8192c91f5cfbb17df286885183fd

FIX: typos and arguments in some Slack apis.
Typos: "conversations" has only one t.
Slack method conversations.open has argument "users" in plural.

conversations.join has no argument "name",
use required "channel" with channel id instead.

slack-channel.c
@@ -269,7 +269,7 @@ void slack_join_chat(PurpleConnection *gc, GHashTable *info) {
 	if (chan && chan->type >= SLACK_CHANNEL_MEMBER)
 		channels_join_cb(sa, join, NULL, NULL);
 	else
-		slack_api_post(sa, channels_join_cb, join, "converstations.join", "name", name, NULL);
+		slack_api_post(sa, channels_join_cb, join, "conversations.join", "channel", chan->object.id, NULL);
 }
 
 void slack_chat_leave(PurpleConnection *gc, int cid) {

slack-im.c
@@ -184,7 +184,7 @@ int slack_im_send(SlackAccount *sa, SlackUser *user, const char *msg, PurpleMess
 	send->thread = g_strdup(thread);
 
 	if (!*user->im)
-		slack_api_post(sa, send_im_open_cb, send, "converstations.open", "user", user->object.id, "return_im", "true", NULL);
+		slack_api_post(sa, send_im_open_cb, send, "conversations.open", "users", user->object.id, "return_im", "true", NULL);
 	else
 		send_im_open_cb(sa, send, NULL, NULL);
 

commit b1592bd3e25f1f74c2190262faedca3dff0b7bbe

avoid a crash introduced in d9c6acd52c3b8192c91f5cfbb17df286885183fd
When a named channel isn't already known, we need a different way to
join it.  For now just produce an error.

Also remove a duplicate g_object_ref(chan)?

slack-channel.c
@@ -259,14 +259,18 @@ void slack_join_chat(PurpleConnection *gc, GHashTable *info) {
 	if (*name == '#')
 		name++;
 	SlackChannel *chan = g_hash_table_lookup(sa->channel_names, name);
-	if (chan)
-		g_object_ref(chan);
+	if (!chan)
+	{
+		/* TODO: should do a better lookup now that conversations.join doesn't take name */
+		purple_notify_error(gc, "Join chat", "Channel not found", name);
+		return;
+	}
+
 	struct join_channel *join = g_new0(struct join_channel, 1);
-	if (chan)
-		join->chan = g_object_ref(chan);
+	join->chan = g_object_ref(chan);
 	join->name = g_strdup(name);
 
-	if (chan && chan->type >= SLACK_CHANNEL_MEMBER)
+	if (chan->type >= SLACK_CHANNEL_MEMBER)
 		channels_join_cb(sa, join, NULL, NULL);
 	else
 		slack_api_post(sa, channels_join_cb, join, "conversations.join", "channel", chan->object.id, NULL);